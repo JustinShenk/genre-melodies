cdcfa071f42565e8958f2bf9e10f791aa6d679c4
Justin Shenk <shenkjustin@gmail.com>
2017-07-27 00:54:31 +0200

    Clean up script

--- train_model.sh
+++ train_model.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+BASE_PATH=$(pwd)/subsets
+
 for genre in */ do convert_dir_to_note_sequences \
-  --input_dir=$genre
-  --output_file="${genre%/}_notesequences.tfrecord"
+  --input_dir=$BASE_PATH/$genre \
+  --output_file=/tmp/${genre%/}_notesequences.tfrecord \
   --recursive;
 done
 
 for genre in */ ; do melody_rnn_create_dataset \
-  --config=attention_rnn
-  --input="$(pwd)/${genre%/}_notesequences.tfrecord"
+  --config=attention_rnn \
+  --input=/tmp/${genre%/}_notesequences.tfrecord \
   --output_dir="sequence_examples/${genre}"
   --eval_ratio=0.10;
   echo "INFO: ${genre%/} database created."
@@ -19,8 +21,8 @@ echo "INFO: Melody database creation completed."
 for genre in */ ;
 do if [[ $genre == *examples* ]]; then continue; fi; melody_rnn_train \
   --config="attention_rnn" \
-  --run_dir=/tmp/melody_rnn/logdir/run1 \
-  --sequence_example_file=$(pwd)/sequence_examples/${genre}training_melodies.tfrecord \
+  --run_dir=/tmp/melody_rnn/logdir/run1/${genre} \
+  --sequence_example_file=$(pwd)/sequence_examples/${genre%/}_training_melodies.tfrecord \
   --num_training_steps=20000;
   echo "INFO: ${genre%/} model trained."
 done
@@ -30,7 +32,7 @@ echo "INFO: Melody data models trained."
 for genre in */ ;
 do if [[ $genre == *examples* ]]; then continue; fi; melody_rnn_generate \
   --config=attention_rnn \
-  --run_dir=/tmp/melody_rnn/logdir/run1 \
+  --run_dir=/tmp/melody_rnn/logdir/run1/${genre} \
   --output_dir=/tmp/melody_rnn/generated/${genre} \
   --num_outputs=10 \
   --num_steps=128 \
