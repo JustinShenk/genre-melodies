6e55b3cc7a6c72eab9635c72f70147c33e102f68
Justin Shenk <shenkjustin@gmail.com>
2017-07-27 11:48:30 +0200

    Add shebang, fix logic of dataset creation

--- create_dataset.py
+++ create_dataset.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from __future__ import print_function
 
 import os
@@ -10,30 +11,33 @@ import numpy as np
 
 from collections import Counter
 
+if not os.path.exists('clean_midi'):
+    # Download the 'Clean MIDI' dataset from http://colinraffel.com/projects/lmd/
+    from six.moves import urllib
+    import StringIO
+    import gzip
+    import tarfile
+    FILE_URL = 'http://hog.ee.columbia.edu/craffel/lmd/clean_midi.tar.gz'
+    response = urllib.request.urlopen(FILE_URL)
+    print("INFO: Downloaded {}".format(FILE_URL))
+    compressedFile = StringIO.StringIO()
+    compressedFile.write(response.read())
+    compressedFile.seek(0)
+    decompressedFile = gzip.GzipFile(fileobj=compressedFile, mode='rb')
+    OUTFILE_PATH = 'clean_midi.tar'
+    with open(OUTFILE_PATH, 'wb') as outfile:
+        outfile.write(decompressedFile.read())
+    tar = tarfile.open(OUTFILE_PATH)
+    tar.extractall()
+    tar.close()
+else:
+    print("INFO: Found `clean_midi` directory")
+
 if not os.path.exists('genres.p'):
     # Login to Spotify and get your OAuth token:
     # https://developer.spotify.com/web-api/search-item/
-    AUTH = "BQD4KKg1opql7Vi8_34OjujPQRhut-beyfo4jcKdPZA7BwPd9MtMx7vSFVzd3K9J_v4LElaZ6B8dCRV5pp0caAusHhLbLAxB1cYuNlhA-9tWLpuheMrlEIQE-ey-q8m5Nevkh83NV6c"
+    AUTH = "ENTER-YOUR-AUTH-KEY"
 
-    if not os.path.exists('clean_midi'):
-        # Download the 'Clean MIDI' dataset from http://colinraffel.com/projects/lmd/
-        from six.moves import urllib
-        import StringIO
-        import gzip
-        import tarfile
-        FILE_URL = 'http://hog.ee.columbia.edu/craffel/lmd/clean_midi.tar.gz'
-        response = urllib.request.urlopen(FILE_URL)
-        print("INFO: Downloaded {}".format(FILE_URL))
-        compressedFile = StringIO.StringIO()
-        compressedFile.write(response.read())
-        compressedFile.seek(0)
-        decompressedFile = gzip.GzipFile(fileobj=compressedFile, mode='rb')
-        OUTFILE_PATH = 'clean_midi.tar'
-        with open(OUTFILE_PATH, 'wb') as outfile:
-            outfile.write(decompressedFile.read())
-        tar = tarfile.open(OUTFILE_PATH)
-        tar.extractall()
-        tar.close()
     # Get artists from folder names
     artists = [item for item in os.listdir(
         'clean_midi') if not item.startswith('.')]
@@ -84,7 +88,7 @@ for genre, artists in genre_data.items():
     try:
         for artist in artists:
             shutil.copytree(os.path.join(MIDI_DIR, artist), os.path.join(
-                os.getcwd(), 'subsets', genre, artist))
+                os.getcwd(), 'subsets', genre.replace(' ', '_'), artist))
     except Exception as e:
         print(e)
     print("INFO: {} folder created.".format(genre))
