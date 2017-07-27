f01df251c408308b5765096b9863f64482f8d884
Justin Shenk <shenkjustin@gmail.com>
2017-07-27 03:15:53 +0200

    Fix bash script

--- train_model.sh
+++ train_model.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
 cd subsets
-BASE_PATH=$(pwd)
 
 for genre in */ ; do convert_dir_to_note_sequences \
   --input_dir=$genre \
   --output_file=/tmp/${genre%/}_notesequences.tfrecord \
-  --recursive;
+  --recursive && echo "INFO: ${genre%/} converted to NoteSequences"
 done
 
-for genre in */ ; do melody_rnn_create_dataset \
+for genre in metal ; do melody_rnn_create_dataset \
   --config=attention_rnn \
   --input=/tmp/${genre%/}_notesequences.tfrecord \
-  --output_dir="sequence_examples/${genre}"
+  --output_dir="sequence_examples/${genre}" \
   --eval_ratio=0.10 && echo "INFO: ${genre%/} database created."
 done
 
@@ -20,25 +19,29 @@ echo "INFO: Melody database creation completed."
 
 for genre in */ ;
 do
-  if [[ $genre == *examples* ]]; then continue;
+if [[ $genre == *examples* ]];
+  then continue;
 fi;
 melody_rnn_train \
-  --config="attention_rnn" \
+  --config=attention_rnn \
   --run_dir=/tmp/melody_rnn/logdir/run1/${genre} \
-  --sequence_example_file=$(pwd)/sequence_examples/${genre%/}_training_melodies.tfrecord \
-  --num_training_steps=20000;
-  echo "INFO: ${genre%/} model trained."
+  --sequence_example_file=$(pwd)/sequence_examples/${genre%/}/training_melodies.tfrecord \
+  --hparams="batch_size=64,rnn_layer_sizes=[64,64]" \
+  --num_training_steps=2000 && echo "INFO: ${genre%/} model trained."
 done
 
 echo "INFO: Melody data models trained."
 
 for genre in */ ;
-do if [[ $genre == *examples* ]]; then continue; fi; melody_rnn_generate \
+do if [[ $genre == *examples* ]];
+  then continue;
+fi;
+melody_rnn_generate \
   --config=attention_rnn \
   --run_dir=/tmp/melody_rnn/logdir/run1/${genre} \
   --output_dir=/tmp/melody_rnn/generated/${genre} \
   --num_outputs=10 \
   --num_steps=128 \
-  --primer_melody="[60]";
-  echo "INFO: ${genre%/} melodies generated."
+  --hparams="batch_size=64,rnn_layer_sizes=[64,64]" \
+  --primer_melody="[60]" && echo "INFO: ${genre%/} melodies generated."
 done
