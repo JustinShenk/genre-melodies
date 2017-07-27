add5035d6b76bba2f6c599cf5bffaaf2024596b2
Justin Shenk <shenkjustin@gmail.com>
2017-07-27 14:21:26 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+#  only need to run this script with the command (do not type the #)
+#  bash setup.sh
+
+echo "First updating the system"
 sudo apt-get update -y && sudo apt-get upgrade -y
-
-sudo apt-get install libasound2-dev libasound-dev libjack-dev -y
-
-curl https://raw.githubusercontent.com/tensorflow/magenta/master/magenta/tools/magenta-install.sh > /tmp/magenta-install.sh
+echo "Installing dependencies"
+sudo apt-get install libasound2-dev libasound-dev libjack-dev git -y && \
+echo "Installing Magenta"
+curl https://raw.githubusercontent.com/tensorflow/magenta/master/magenta/tools/magenta-install.sh > /tmp/magenta-install.sh &&
 bash /tmp/magenta-install.sh
 
-source activate magenta
-pip install tensorflow-gpu
+echo "Open a new terminal window so the environmental variable changes take effect and enter:"
+echo "  echo 'source activate magenta && pip install tensorflow-gpu &&
+git clone https://github.com/JustinShenk/genre-melodies.git &&
+cd genre-melodies &&
+pip install -r requirements.txt &&
+python create_dataset.py &&
+sh train_model.sh'"
